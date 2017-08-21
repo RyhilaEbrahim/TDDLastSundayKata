9f022ec4316cc400b427d9a3921b8a9de451af62
Ryhila Ebrahim <ryhila15@gmail.com>
2017-08-21 14:42:06 +0200

    Initial commit

---
+++ LastSundayKata.Tests/LastSundayKataTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LastSundayKata.Tests
+{
+    [TestFixture]
+    public class LastSundayKataTests
+    {
+        [Test]
+        public void LastSunday_GivenYear2013_ShouldReturnLastSundayForJanuary()
+        {
+            //---------------Set up test pack-------------------
+            var sut = Create();
+            var expected = new DateTime(2013,1,27);
+            var input = new DateTime(2013,1,1);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var result = sut.LastSunday(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result.First());
+        }
+
+        [Test]
+        public void LastSunday_GivenYear2013_ShouldReturnListOfLastSundaysForThatYear()
+        {
+            //---------------Set up test pack-------------------
+            var sut = Create();
+            var expected = ExpectedYears();
+            var input = new DateTime(2013,1,1);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var result = sut.LastSunday(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected,result);
+        }
+
+        [Test]
+        public void LastSunday_GivenYear2020_ShouldReturnListOfLastSundayForFebruary()
+        {
+            //---------------Set up test pack-------------------
+            var sut = Create();
+            var expected = ExpectedYears();
+            var input = new DateTime(2020,1,1);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var result = sut.LastSunday(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(new DateTime(2020,2,23), result[1]);
+        }
+
+        [Test]
+        public void LastSunday_GivenYear2023_ShouldReturnListOfLastSundayForFebruary()
+        {
+            //---------------Set up test pack-------------------
+            var sut = Create();
+            var expected = ExpectedYears();
+            var input = new DateTime(2032,1,1);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var result = sut.LastSunday(input);
+            //---------------Test Result -----------------------
+            Assert.AreEqual(new DateTime(2032,2,29), result[1]);
+        }
+
+        private List<DateTime> ExpectedYears()
+        {
+            return new List<DateTime>
+            {
+                new DateTime(2013, 1, 27),
+                new DateTime(2013, 2, 24),
+                new DateTime(2013, 3, 31),
+                new DateTime(2013, 4, 28),
+                new DateTime(2013, 5, 26),
+                new DateTime(2013, 6, 30),
+                new DateTime(2013, 7, 28),
+                new DateTime(2013, 8, 25),
+                new DateTime(2013, 9, 29),
+                new DateTime(2013, 10, 27),
+                new DateTime(2013, 11, 24),
+                new DateTime(2013, 12, 29),
+            };
+        }
+
+        private  LastSundayKata Create()
+        {
+            return new LastSundayKata();
+        }
+    }
+}
---
+++ LastSundayKata/LastSundayKata.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LastSundayKata
+{
+    public class LastSundayKata
+    {
+        public List<DateTime> LastSunday(DateTime input)
+        {
+            var lastSundays = GetListOfLastSundaysFor(input.Year);
+            return lastSundays;
+        }
+
+        private List<DateTime> GetListOfLastSundaysFor(int year)
+        {
+            var date = GetNewDateTimeList();
+            for (var month = 1; month <= 12; month++)
+            {
+                var lastSundayForMonth = GetLastSundayForCurrentMonth(year, month);
+                date.Add(lastSundayForMonth);
+            }
+            return date;
+        }
+
+        private DateTime GetLastSundayForCurrentMonth(int year, int month)
+        {
+            var lastSundaysOfCurrentMonth = GetNewDateTimeList();
+            var lastDayInMonth = GetNumberOfDaysInMonth(year, month);
+            for (var day = 1; day <= lastDayInMonth; day++)
+            {
+                var dateOfCurrentMonth = GetNewDateTimeFor(year, month, day);
+                if (IsSunday(dateOfCurrentMonth))
+                {
+                    lastSundaysOfCurrentMonth.Add(dateOfCurrentMonth);
+                }
+            }
+            return lastSundaysOfCurrentMonth.LastOrDefault();
+        }
+
+        private  List<DateTime> GetNewDateTimeList()
+        {
+            return new List<DateTime>();
+        }
+
+        private DateTime GetNewDateTimeFor(int year, int month, int day = 1)
+        {
+            return new DateTime(year, month, day);
+        }
+
+        private bool IsSunday(DateTime dateOfCurrentMonth)
+        {
+            return GetDayOfWeek(dateOfCurrentMonth) == DayOfWeek.Sunday;
+        }
+
+        private  DayOfWeek GetDayOfWeek(DateTime dateOfCurrentMonth)
+        {
+            return dateOfCurrentMonth.DayOfWeek;
+        }
+
+        private  int GetNumberOfDaysInMonth(int year, int month)
+        {
+            return DateTime.DaysInMonth(year, month);
+        }
+    }
+}
